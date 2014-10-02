c56a1c5ee2b8d18de6f940e7b860a8cc8ecfa9fb
sacha67000 <oleksandr.avazashvili@epitech.eu>
2014-10-02 17:05:01 +0200

    Menu Done

---
+++ Project/Assets/Standard Assets/My_Scripts/ChangeTextStart.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+function Start () {
+	this.guiText.pixelOffset = Vector2((Screen.width * 0.50) - 100, 10);
+}
+
+var couleurEntrer : Color = Color.green;
+var couleurSortie : Color = Color.white;
+var tailleEntrer : float = 80;
+var tailleSortie : float = 70;
+
+function OnMouseEnter() {
+	guiText.material.color = couleurEntrer;
+	guiText.fontSize = tailleEntrer;
+}
+
+function OnMouseExit() {
+	guiText.material.color = couleurSortie;
+	guiText.fontSize = tailleSortie;
+}
+
+function OnMouseUp() {
+// Application.LoadLevel(levelSuivant);
+}
