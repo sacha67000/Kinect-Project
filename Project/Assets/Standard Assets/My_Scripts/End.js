b406fc40d693e78fbff07a3f0a023d31cd55f7b4
sacha67000 <oleksandr.avazashvili@epitech.eu>
2014-10-09 14:03:46 +0200

    New New /

---
+++ Project/Assets/Standard Assets/My_Scripts/End.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Reload()
+{
+	yield WaitForSeconds(1);
+	Application.LoadLevel(Application.loadedLevel);
+}
+
+function OnTriggerEnter (other : Collider)
+{
+	Reload();
+}
